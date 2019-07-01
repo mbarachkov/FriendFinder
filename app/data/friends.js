75a9b6ac5ed07763ec2ebf390a67077bd20fb566
Michael Barachkov <mike.barachkov@outlook.com>
2019-06-30 20:24:24 -0700

    did the argument for adding and comparing friends

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,10 +1,4 @@
-// ===============================================================================
-// DATA
-// Below data will hold all of the reserved tables.
-// Initially we just set it equal to a "dummy" customer.
-// But you could have it be an empty array as well.
-// ===============================================================================
-//tableArray old 
+
 const friendsArr = [
   {
     name: "Ahmed",
@@ -29,5 +23,4 @@ const friendsArr = [
   }
 ];
 
-// Note how we export the array. This makes it accessible to other files using require.
 module.exports = friendsArr;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,5 +1,5 @@
 
-const friends = require("../data/friends");
+const friends = require("../data/friends.js");
 
 module.exports = (app) => {
 
@@ -7,6 +7,30 @@ module.exports = (app) => {
     res.json(friends);
   });
 
+  app.post("/api/friends", function (req, res) {
+    const match = {
+      name: "",
+      photo: "",
+      pointDiff: 1000
+    }
+    var userData = req.body;
+    var userScores = userData.scores;
+    var totalDiff = 0;
+    for (var i = 0; i < friends.length - 1; i++) {
+      console.log(friends[i].name)
+      totalDiff = 0;
+      for (var x = 0; x < 10; x++){
+        totalDiff += Math.abs(parseInt(userScores[x]) - parseInt(friends[i].scores[x]));
+        if (totalDiff <= match.pointDiff){
+          match.name = friends[i].name;
+          match.photo = friends[i].photo;
+          match.pointDiff = totalDiff;
+        }
+      }
+    }
+    friends.push(userData);
+    res.json(match);
+  });
 
 
   app.post("/api/clear", (req, res) => {
